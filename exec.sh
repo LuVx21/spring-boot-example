d9198294892b50c956b83c2112d3ebd4619b731d
RenXie <yeyu0926@hotmail.com>
2019-04-17 22:57:16 +0800

    update

--- exec.sh
+++ exec.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-# author: 13
-# date: 2017-06-30
+# author: Ren, Xie
 
-
-env_args="-Xms128m -Xmx128m"
+# env_args="-Xms128m -Xmx128m"
+env_args=""
 sleeptime=0
 arglen=$#
+jarname='example-executable.jar'
 
-# get luvx pid
 get_pid(){
-    pname="`find .. -name 'spring-boot-example-1.0.1-SNAPSHOT.jar'`"
+    pname="`find ./ -name $jarname`"
     pname=${pname:3}
     pid=`ps -ef | grep $(basename $pname) | grep -v grep | awk '{print $2}'`
     echo "$pid"
 }
 
 startup(){
-
-    #构建并启动
-    mvn clean install -Dmaven.test.skip=true
+    mvn clean package -DskipTests
+    # mvn clean install -Dmaven.test.skip=true
     pid=$(get_pid)
     if [ "$pid" != "" ]
     then
-        echo "Blog already startup!"
+        echo "APP already startup!"
     else
-        jar_path=`find .. -name 'spring-boot-example-1.0.1-SNAPSHOT.jar'`
-        echo "jarfile=$jar_path"
-        cmd="java $1 -jar $jar_path > ./luvx.out < /dev/null &"
+        jar_path=`find ./ -name $jarname`
+        cmd="nohup java $1 -jar $jar_path > ./example.out < /dev/null &"
         echo "cmd: $cmd"
-        java $1 -jar $jar_path > ./luvx.out < /dev/null &
+        nohup java $1 -jar $jar_path > ./example.out < /dev/null &
+        pid=$(get_pid)
+        echo "pid: $pid" >> ./example.out
         show_log
     fi
 }
@@ -37,31 +36,38 @@ shut_down(){
     pid=$(get_pid)
     if [ "$pid" != "" ]
     then
-        kill -9 $pid
-        echo "Blog is stop!"
+        curl -X POST http://localhost:58080/MyActuator/shutdown
+        pid=$(get_pid)
+        if [ "$pid" != "" ]
+        then
+            kill -9 $pid
+            echo -e "\r\nkill process!"
+        else
+            echo -e "\r\nAPP is stop!"
+        fi
     else
-        echo "Blog already stop!"
+        echo "APP is not running!"
     fi
 }
 
 show_log(){
-    tail -f luvx.out
-}
-
-show_help(){
-    echo -e "\r\n\t欢迎使用Blog"
-    echo -e "\r\nUsage: sh exec.sh start|stop|reload|status|log"
-    exit
+    tail -f example.out
 }
 
 show_status(){
     pid=$(get_pid)
     if [ "$pid" != "" ]
     then
-        echo "Blog is running with pid: $pid"
+        echo "APP is running with pid: $pid"
     else
-        echo "Blog is stop!"
+        echo "APP is not running!"
     fi
+}
+
+show_help(){
+    echo -e "\r\n\t欢迎使用APP"
+    echo -e "\r\nUsage: sh exec.sh start|stop|reload|status|log|pid"
+    exit
 }
 
 if [ $arglen -eq 0 ]
@@ -73,6 +79,9 @@ else
         env_args="$2"
     fi
     case "$1" in
+        "pid")
+            get_pid
+            ;;
         "start")
             startup "$env_args"
             ;;
